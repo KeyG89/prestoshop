6d81eaa6704d19678b1cced0549a03de549e1e34
KeyG89 <krzysztof.goscinski@gmail.com>
2018-06-06 00:21:35 +0200

    <S8>(MVP)Add CheckoutContract Interface implemented in mvp manner -> View/Action/Repository interfaces

---
+++ app/src/main/java/mobile/viali/prontoshop/ui/checkout/CheckoutContract.java
@@ -0,0 +1,60 @@
+package mobile.viali.prontoshop.ui.checkout;
+
+import java.util.List;
+
+import mobile.viali.prontoshop.model.LineItem;
+import mobile.viali.prontoshop.model.Transaction;
+
+public interface CheckoutContract {
+
+    public interface View {
+
+        void showLineItem(List<LineItem> items);
+
+        void showEmptyText();
+
+        void showCartTotals(double tax, double subTotal, double total);
+
+        void showConfirmCheckout();
+
+        void showConfirmClearChart();
+
+        void hideText();
+
+        void showMessage(String message);
+
+
+    }
+
+    public interface Action {
+        void loadLineItems();
+
+        void onCheckoutButtonClicked();
+
+        void onDeleteItemButtonClicked(LineItem item);
+
+        void checkout();
+
+        void onClearButtonClicked();
+
+        void clearShoppingCart();
+
+        void setPaymentType(String paymentType);
+
+        void markAsPaid(boolean paid);
+
+        void onItemQuantityChanged(LineItem item, int qty);
+
+    }
+
+    public interface Repository {
+
+        List<LineItem> getAllLineItems();
+
+        void saveTransaction(Transaction transaction);
+
+        void updateTransaction(Transaction transaction);
+
+
+    }
+}
