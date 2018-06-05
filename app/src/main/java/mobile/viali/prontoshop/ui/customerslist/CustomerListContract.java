41bc2a9a447af15075466985e66a7446dc9bd950
KeyG89 <krzysztof.goscinski@gmail.com>
2018-06-06 00:14:37 +0200

    <S8>(MVP)Add CustomerListContract Interface implemented in mvp manner -> View/Action/Repository interfaces

---
+++ app/src/main/java/mobile/viali/prontoshop/ui/customerslist/CustomerListContract.java
@@ -0,0 +1,63 @@
+package mobile.viali.prontoshop.ui.customerslist;
+
+import java.util.List;
+
+import mobile.viali.prontoshop.model.Customer;
+
+public interface CustomerListContract {
+
+    public interface View {
+
+        void showCustomers(List<Customer> customers);
+
+        void showAddCustomerForm();
+
+        void showDeleteCustomerPrompt(Customer customer);
+
+        void showEditCustomerForm(Customer customer);
+
+        void showEmptyText();
+
+        void hideEmptyText();
+
+        void showMessage(String message);
+
+    }
+
+    public interface Action {
+
+        void loadCustomer();
+
+        Customer getCustomer(long id);
+
+        void onCustomerSelected(Customer customer);
+
+        void onAddCustomerButtonClicked();
+
+        void addCustomer(Customer customer);
+
+        void onDeleteCustomerButtonClicked(Customer customer);
+
+        void deleteCustomer(Customer customer);
+
+        void onEditCustomerButtonNClicked(Customer customer);
+
+        void updateCustomer(Customer customer);
+
+    }
+
+    public interface Repository {
+
+        List<Customer> getAllCustomers();
+
+        Customer getCustomerById(long id);
+
+        void onDeleteCustomer(Customer customer);
+
+        void addCustomer(Customer customer);
+
+        void updateCustomer(Customer cusomter);
+
+
+    }
+}
